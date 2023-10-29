d7449b67d4bf26a952594128c38e05ad146f996a
AlrMel <alexandrm1233@gmail.com>
2023-10-29 23:15:58 +0200

    Step-01_b

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  loginInputId = nanoid();
+
+  handeleContact = event => {
+    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
+  };
+
+
+
+  render() {
+    const {contacts, name} = this.state;
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <form>
+          <label>
+            Name:
+            <input
+              onChange={this.handeleContact}
+              id={this.loginInputId}
+              value={this.state.name}
+              type="text"
+              name="name"
+              required
+            />
+          </label>
+          <label>Number
+            <input type="tel" />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={name}>{contact.name}</li>
+          ))}
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
